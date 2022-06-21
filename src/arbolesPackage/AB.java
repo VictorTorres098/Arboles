43aa925bb24a8e292940cb1896b66bf61f3bfbd1
bach <byebachgm@gmail.com>
2022-06-21 15:22:55 -0300

    finals

--- src/arbolesPackage/AB.java
+++ src/arbolesPackage/AB.java
@@ -3,13 +3,31 @@ package arbolesPackage;
 public class AB <T> {
 	private Nodo <T> raiz;
 	
-	public class Nodo <T>{
+	public static class Nodo <T>{
 		private T info;
 		private Nodo<T> izq;
 		private Nodo<T> der;
 		
-		public Nodo(T info) {
+		public Nodo(T elem) {
+			info = elem;
+		}
+		public T getInfo() {
+			return info;
+		}
+		public void setInfo(T info) {
 			this.info = info;
+		}
+		public Nodo<T> getIzq(){
+			return izq;
+		}
+		public void setIzq(Nodo<T> izq) {
+			this.izq = izq;
+		}
+		public Nodo<T> getDer(){
+			return der;
+		}
+		public void setDer(Nodo<T> izq) {
+			this.der = izq;
 		}
 		@Override
 		public String toString() {
@@ -26,95 +44,204 @@ public class AB <T> {
 			agregar(raiz, nuevo);
 		}
 	}
-	private void agregar(Nodo<T> padre, Nodo<T> nuevo) {
-		if(padre.izq == null) {								//Si no tiene nada en su hijo izq agrega el nuevo elemento a izq	
-			padre.izq = nuevo;
+	protected void agregar(Nodo<T> padre, Nodo<T> nuevo) {
+		if(padre.getIzq() == null) {							//Si no tiene nada en su hijo izq agrega el nuevo elemento a izq	
+			padre.setIzq(nuevo);
 		}else {												
-			if(padre.der == null) {							//Si izq !null lo agrego al hijo der
-				padre.der = nuevo;
+			if(padre.getDer() == null) {							//Si izq !null lo agrego al hijo der
+				padre.setDer(nuevo);
 			}else {											//si ambos no son !null se fija en los hijo izq y der de der
-				agregar(padre.der , nuevo);              	//Desicion de implementacion: generar el arbol a derecha
+				agregar(padre.getDer() , nuevo);              	//Desicion de implementacion: generar el arbol a derecha
 			}
 		}
 	}
 	
-	public Nodo<T> buscar(T elem){
-		return (raiz == null) ? null : buscar(raiz, elem);
+	public boolean buscar(T elem){
+		if(raiz == null)
+			return false;
+		return buscar(raiz, elem);
 	}
-	private Nodo<T> buscar(Nodo<T> nodo, T elem){
-		if(nodo.info.equals(elem)) {
-			return nodo;
-		}else {
-			Nodo<T> izq = null;
-			Nodo<T> der = null;
-			if(nodo.izq !=null) {
-				izq = buscar(nodo.izq, elem);
-			}
-			if(nodo.der != null) {
-				der = buscar(nodo.der, elem);
-			}
-			//decision de implementacion: si esta en ambos lados, mostramos al izquierdo primero
-			if(izq != null ) {
-				return izq;
-			}else {
-				return der;
-			}
-		}
+	
+	protected boolean buscar(Nodo<T> nodo, T elem){
+		if(nodo == null)
+			return false;
+		if(nodo.getInfo().equals(elem))
+			return true;
+		return buscar(nodo.getDer(), elem) || buscar(nodo.getIzq(), elem);
 	}
-	public int altura() {
+	public int cantNodos() {
+		return(raiz == null) ? 0 : cantNodos(raiz);
+	}
+	//complejidad O(N) porque para el pero caso hay que recorrer todos los nodos
+	private int cantNodos(Nodo<T> nodo) {
+		int cantIzq = (nodo.getIzq() == null ) ? 0 : cantNodos(nodo.getIzq());
+		int cantDer = (nodo.getDer() == null) ? 0 : cantNodos(nodo.getDer());
+		
+		return 1 + cantIzq + cantDer;
+	}
+	
+	//cada operacion de altura() es O(1)
+	//se producen una llamada recursiva por cada nodo
+	//entonces se produce O(1).n = O(N)
+	
+ 	public int altura() {
 		return (raiz == null) ? 0 : altura(raiz);
 	}
 	private int altura(Nodo<T> nodo) {
+		//busco la altura recursivamente de cada lado
 		int altIzq = (nodo.izq == null) ? 0 : altura(nodo.izq);
 		int altDer = (nodo.der == null) ? 0 : altura(nodo.der);
+		//+1 porque suma la altura del padre de todos
 		return 1 + Math.max(altIzq, altDer);
 	}
 	//version 1
 	public boolean balanceado() {
-		if(raiz == null) {
-			return true;
-		}else {
-			return balanceado(raiz);
-		}
+		return(raiz == null) ? true : balanceado(raiz);
 	}
-	private boolean balanceado(Nodo<T> raiz) {
-		boolean ret = true;
-		int altIzq = 0;
-		int altDer = 0;
-		if(raiz.izq != null) {
-			altIzq = altura(raiz.izq);
-			ret = ret && balanceado(raiz.izq);
+	//la complejidad de balanceado es O(n2) ya que en cada nodo que te pares, tenes que ver la altura
+		//y a la vez tenes que pasar por todos los nodos, osea debes de ver la altura de todos los nodos
+		//O(n)*O(n) = O (n2)
+		private boolean balanceado(Nodo<T> nodo) {
+			boolean ret = true;
+			int altIzq = 0;
+			int altDer = 0;
+			
+			if(nodo.getIzq() != null) {
+				altIzq = altura(nodo.getIzq());
+				ret = ret && balanceado(nodo.getIzq());
+			}
+			
+			if(nodo.getDer() != null) {
+				altDer = altura(nodo.getDer());
+				ret = ret && balanceado(nodo.getDer());
+			}
+			
+			ret = ret && Math.abs(altIzq - altDer) <= 1;
+			
+			return ret;
 		}
-		if(raiz.der != null) {
-			altDer = altura(raiz.der);
-			ret = ret && balanceado(raiz.der);
-		}
-		ret = ret && Math.abs(altIzq - altDer) <= 1;
-		return ret;
-	}
 	//version2: a verces convien hacer el caso base sobre el nodo
-	public boolean balanceadoB() {
-		return balanceadoB(raiz);
+	public boolean balanceado2() {
+			return balanceado2(raiz);
 	}
-	private boolean balanceadoB(Nodo<T> nodo) {
-		if(nodo == null) return true;
-		else {
-			int altIzq = (nodo.izq == null) ? 0 : altura(nodo.izq);
-			int altDer = (nodo.der == null) ? 0 : altura(nodo.der);
-			return Math.abs(altIzq - altDer) <= 1 && balanceadoB(nodo.izq) && balanceadoB(nodo.der);
+		
+	private boolean balanceado2(Nodo<T> nodo) {
+			if(nodo == null)
+				return true;
+			
+			else {
+				int altIzq = (nodo.getIzq() == null) ? 0 : altura(nodo.getIzq());
+				int altDer = (nodo.getDer() == null) ? 0 : altura(nodo.getDer());
+				
+				return Math.abs(altIzq - altDer) <= 1 && balanceado(nodo.getIzq()) &&
+				       balanceado(nodo.getDer());
+			}
+				
+	}
+	public int cantHojas() {
+		if(raiz == null) 
+			return 0;
+		return cantHojas(raiz);
+	}
+	private int cantHojas(Nodo<T> nodo) {
+		if(nodo == null)
+			return 0;
+		if(nodo.getIzq() == null && nodo.getDer() == null)
+			return 1;
+		return cantHojas(nodo.getDer()) + cantHojas(nodo.getIzq());
+	}
+	public Nodo<T> getRaiz(){
+		return raiz; 
+	}
+	public int getValorRaiz() {
+		int raizNum = (int) raiz.getInfo();
+		return raizNum;
+	}
+	
+	public int sumaInternos() {
+		if(raiz == null)
+			return 0;
+		
+		return sumaInternos((Nodo<Integer>) raiz);
+	}
+	private int sumaInternos(Nodo<Integer> nodo) {
+		if(nodo == null)
+			return 0;
+		if(nodo.equals(getRaiz()))
+			return sumaInternos(nodo.getIzq()) + sumaInternos(nodo.getDer());
+		if(!esHoja(nodo))
+			return sumaInternos(nodo.getIzq()) + sumaInternos(nodo.getDer());
+		return sumaInternos(nodo.getIzq()) + sumaInternos(nodo.getDer());
+	}
+	//es O(n) porque se tiene que recorrer todos los nodos para determinar cuales son las hojas
+	//y sumarlos, y la operacion esHoja() es O(1) que se debe realizar por cada mpdp osea
+	//es(1) * n nodos, O(N)
+	private boolean esHoja(Nodo<Integer> nodo) {
+		if(nodo.getIzq() == null && nodo.getDer() == null)
+			return true;
+		else
+			return false;
+	}
+	public static boolean esABB(AB<Integer> ab) {
+		if(ab.getRaiz() == null)
+			return false;
+		return esABB(ab.getRaiz());
+	}
+	//bonus track (0,5)
+	//Decir cual es la complejidad del metodo en a. Explicar si cambia la complejidad
+	//cuando el arbol esta balanceado. Justificando la respuesta.
+	
+	//es O(N) que tengo que recorrer todos los nodos para corroborrar que se cumpla la condicion de ANN
+	//no cambia la complejidad si esta balanceado o no ya que en cualquier caso debo verificar que todos los 
+	//subarboles del original sean arboles de busqueda binario, es decir que por cada nodo debo verificar que sea arbol binario de busqueda
+	private static boolean esABB(Nodo<Integer> nodo) {
+		if(nodo == null)
+			return true;
+		
+		if(nodo.getIzq()!= null && nodo.getDer()!= null) {
+			if(nodo.getInfo().compareTo(nodo.getIzq().getInfo()) > 0
+			&& nodo.getInfo().compareTo(nodo.getDer().getInfo()) < 0)
+				return true && esABB(nodo.getIzq()) && esABB(nodo.getDer());
+			
+			return false;
 		}
+		return true;
 	}
-	@Override
+	//retorna un String con todos los elementos del Arbol, siguiendo el recorrido inOrden
 	public String toString() {
-		return (raiz == null) ? " " : toString(raiz);
+		return "{" + toStringInorden(raiz) + "]";
 	}
-	private String toString(Nodo<T> nodo) {
-		String ret = nodo.info.toString();
-		if(nodo.izq != null) ret = ret + toString(nodo.izq);
-		if(nodo.der != null) ret = ret + toString(nodo.der);
+	private String toStringInorden(Nodo<T> nodo) {
+		StringBuilder sb = new StringBuilder();
+		if(nodo!= null) {
+			sb.append(toStringInorden(nodo.getDer()));
+			sb.append(nodo.getInfo()).append(" ");
+			sb.append( toStringInorden(nodo.getIzq()) );
+		}
+		return sb.toString();
+	}
+	public String preOrden() {
+		return "{" + toStringPreorden(raiz) + "}";
+	}
+	
+	private String toStringPreorden(Nodo<T> nodo) {
+		// TODO Auto-generated method stub
+		StringBuilder sb = new StringBuilder();
+		if (nodo!=null) {
+			sb.append(nodo.getInfo()).append(" ");
+			sb.append( toStringPreorden(nodo.getIzq()) );
+			sb.append( toStringPreorden(nodo.getDer()) );
+		}
+		return sb.toString();
+	}
+	public String toString2(){
+		return (raiz == null) ? "" : toString2(raiz);
+	}
+	
+	private String toString2(Nodo<T> nodo){
+		String ret = nodo.getInfo().toString();
+		if (nodo.getIzq() != null) ret = ret + toString2(nodo.getIzq());
+		if (nodo.getDer() != null) ret = ret + toString2(nodo.getDer());
 		return ret;
-	}
-	
-	
-	
+	}	
 }
--- src/arbolesPackage/ABB.java
+++ src/arbolesPackage/ABB.java
@@ -1,111 +1,202 @@
 package arbolesPackage;
 
-import arbolesPackage.AB.Nodo;
+import java.util.ArrayList;
 
-public class ABB <T extends Comparable<T>> {
+public class ABB<T extends Comparable<T>> extends AB<T>{
+	//Complejidad logaritmica, siempre te quedas con la mitad cada vez O(Log(N)) si esta balanceado
+	//Si esta desbalanceado puede ser hasta una lista(todos los nodos de un solo lado) Y llegar a ser O(N)
 	
-	Nodo<T> raiz;
+	//para ello se crearon algoritmos de rebalanceo
 	
-	protected class Nodo<T>{ //clase interna
+	//Tiene que estar protected en esta clase como en la de AB porque si esta privada no lo puede veer al metodo
+	//pero es necesario sobreecribirlo, entonces es necesario ponerlos en protected
+	@Override
+	protected boolean buscar(Nodo<T> nodo, T elem) {
+		if(nodo==null)
+			return false;
 		
-		private T info;
-		private Nodo<T> hijoIzq;
-		private Nodo<T> hijoDer;
+		if(elem.equals(nodo.getInfo()))
+			return true;
+		//llama recursiva
+		if(elem.compareTo(nodo.getInfo()) < 0) //si es menor q 0 significa que la de la izq < que lo que esta a la der
+			return buscar(nodo.getIzq(), elem);
 		
-		public Nodo(T info) {
-			super();
-			this.info = info;
-		}
-		
-		public T getInfo() {
-			return info;
-		}
-		
-		public void setInfo(T dato) {
-			this.info = info;
-		}
-		
-		public Nodo<T> getHijoIzq() {
-			return hijoIzq;
-		}
-		
-		public void setHijoIzq(Nodo<T> hijoIzq) {
-			this.hijoIzq = hijoIzq;
-		}
-		
-		public Nodo<T> getHijoDer() {
-			return hijoDer;
-		}
-		public void setHijoDer(Nodo<T> hijoDer) {
-			this.hijoDer = hijoDer;
-		}
-		//fin de subclase
+		return buscar(nodo.getDer(), elem); //calcula las rama derecha 
 	}
-	//nodo raiz
-	public ABB() {
-	}
-	
-	public boolean vacio() {
-		return raiz == null;
-	}
-	
-	public void agregar(T elem) {
-		Nodo<T> nuevo = new Nodo<T>(elem);
-		if (raiz==null)
-			raiz = nuevo;
+	@Override
+	protected void agregar(Nodo<T> nodo, Nodo<T> nuevo) {
+		if(nuevo.getInfo().compareTo(nodo.getInfo()) < 0)
+			if(nodo.getIzq() == null)
+				nodo.setIzq(nuevo);
+			else
+				agregar(nodo.getIzq(), nuevo);
 		else
-			agregar(nuevo,raiz);
+			if(nuevo.getInfo().compareTo(nodo.getInfo()) > 0)
+				if(nodo.getDer() == null) 
+					nodo.setDer(nuevo);
+				else
+					agregar(nodo.getDer(), nuevo); //en este caso no va haber elementos repetidos, por eso 
+												//en caso de ser igual no se agrega el elem
+	}
+	public int nivel(T elem) {
+		if(getRaiz() == null || !buscar(elem)) //si el arbol esta vacio o El elemento no esta
+			return -1;							//retorna -1
+		return nivel(elem,getRaiz());			
+	}
+	private int nivel(T elem, Nodo<T> nodo) {
+		if(nodo.getInfo().equals(elem))			//si el elemento esta en la raiz 
+			return 1;							//retorna 1
+		if(nodo.getInfo().compareTo(elem) < 0)	//si ele elemento es menor que el nodo actual busca en su hijo der
+			return 1 + nivel(elem, nodo.getDer()); //llamada recursiva pero va sumando uno cada vez que baja de nivel	
+		else
+			return 1 + nivel(elem, nodo.getIzq()); //el mismo proceso se repite aca 
+	}	//al retornar suma los +1 de todos los returns y nos devuelve en que nivel se encuentra el elemento
+	
+	public T minimo() {
+		if(getRaiz() == null)
+			return null;
+		return minimo(getRaiz());
+	}
+	private T minimo(Nodo<T> nodo) {
+		if(nodo.getIzq() == null)
+			return nodo.getInfo();
+		return minimo(nodo.getIzq());	//llega hasta el ultimo nodo izq el cual seria el nodo con menor valor
+	}
+	public T maximo() {
+		if(getRaiz() == null) 
+			return null;
+		return maximo(getRaiz());
+	}
+	private T maximo(Nodo<T> nodo) {
+		if(nodo.getDer() == null)
+			return nodo.getInfo();
+		return maximo(nodo.getDer()); //llega hasta el ultimo nodo der el cual seria el nodo con mayor valor
+	}
+	// Un metodo que retorne el elemento en la posicion i,
+	// segun el recorrido inorden
+	// Si el i no es un valor valido, retornar null
+	// El minimo elemento es con i=0
+	public T iesimo(int i) {
+		if(i < 0 || i > cantNodos() - 1)
+			return null;
+		if(i == 0)
+			return minimo();
+		else
+			return iesimo(getRaiz(), i);
+	}
+	private T iesimo(Nodo<T> nodo, int i) {
+		return null;
+	}
+	// Dados dos elementos devuelve los valores de los nodos
+	// que hay entre ellos (incluidos los bordes)
+	// o {} si alguno no pertenece
+	// x ej 
+	// unABB.elemsEntre(1, 5) = {12345}
+	public String elemsEntre(T elem1, T elem2) {
+		if(!buscar(elem1) || !buscar(elem2))
+			return "{}";
+		if(getRaiz() == null)
+			return null;
+		
+		return "{" + elemsEntre(elem1, elem2, getRaiz()) + "}";
+		
 	}
 
-	private void agregar(Nodo<T> nuevo, Nodo<T> padre) {
-		if (nuevo.getInfo().compareTo(padre.getInfo())<0)
-			if (padre.getHijoIzq()==null)
-				padre.setHijoIzq(nuevo);
-			else
-				agregar(nuevo,padre.getHijoIzq());
-		else if (nuevo.getInfo().compareTo(padre.getInfo())>0)
-			if (padre.getHijoDer()==null)
-				padre.setHijoDer(nuevo);
-			else
-				agregar(nuevo,padre.getHijoDer());
+	private String elemsEntre(T elem1, T elem2, Nodo<T> nodo) {
+		StringBuilder sb = new StringBuilder();
+
+		if (nodo != null) {
+			
+			sb.append(elemsEntre(elem1, elem2, nodo.getIzq()));
+			
+			if (nodo.getInfo().compareTo(elem2) <= 0 && nodo.getInfo().compareTo(elem1) >= 0) {
+				
+				sb.append(nodo.getInfo());
+				sb.append(" ");
+				sb.append(elemsEntre(elem1, elem2, nodo.getDer()));
+			}
+		}
+		return sb.toString();
 	}
-	
-	public int cantidadDeNodos() {	
-		return cantidadDeNodos(raiz);
-	}
-	
-	private int cantidadDeNodos(Nodo<T> raiz) {
-		if(raiz == null) {
+	// dado un valor devuelve la cantidad de elementos de la subrama a la que pertenece elem
+	// o 0 en caso contrario
+	public int elemsEnSubrama(T elem) {
+		if (!this.buscar(elem))
 			return 0;
-		}else {
-			return 1 + cantidadDeNodos(raiz.getHijoIzq()) + cantidadDeNodos(raiz.getHijoDer());
-		}
+		return elemsEnSubrama(getRaiz(), elem);
 	}
-	
-	protected Nodo<T> eliminar(T elem, Nodo<T> nodo) {
-		if(nodo == null) {
-			return null
-		}
-	}
-	
-//	public boolean buscar( T valor) {
-//		
-//		if(raiz == null) {
-//			return false;
-//		}else {
-//			return buscar(raiz, valor);
-//		}
-//	}
-//	
-//	private boolean buscar(Nodo<T> raiz, T valor) {
-//		if(raiz.getInfo().compareTo(valor) < 0) {
-//			return buscar(raiz.getHijoIzq(), valor);
-//		}
-//		if(raiz.getInfo().compareTo(valor) > 0) {
-//			return buscar(raiz.getHijoDer(), valor);
-//		}
-//		return raiz.getInfo().compareTo(valor) == 0;
-//	}
+	private int elemsEnSubrama(Nodo<T> actual, T elem) {
 		
+		if(actual == null)
+			return 0;
+		
+		if(actual.getInfo().compareTo(elem) < 0)
+			return 1 + elemsEnSubrama(actual.getDer(), elem) + elemsEnSubrama(actual.getIzq(), elem);
+			
+		return elemsEnSubrama(actual.getDer(), elem) + elemsEnSubrama(actual.getIzq(), elem);
+	}
+	//retorna la suma de los elementos en el arbol que son mayores que la diferencia entre mayor y menor elemento
+	public int sumaMoayoresDifExtremos() {
+		if(getRaiz() == null)
+			return 0;
+		else if(getRaiz() != null && cantNodos() == 1) {
+			return (int) getRaiz().getInfo();
+		}
+		int minimoABB = (int) minimo();
+		int maximoABB = (int) maximo();
+		return sumaMayoresDifExtremos(getRaiz(), minimoABB - maximoABB);
+	}
+	private int sumaMayoresDifExtremos(Nodo<T> nodo, int difExtremos) {
+		if(nodo == null)
+			return 0;
+		int valorNodo = (int) nodo.getInfo();
+		if( valorNodo > difExtremos)
+			return valorNodo + sumaMayoresDifExtremos(nodo.getIzq(), difExtremos) + sumaMayoresDifExtremos(nodo.getDer(), difExtremos);
+		return sumaMayoresDifExtremos(nodo.getIzq(), difExtremos) + sumaMayoresDifExtremos(nodo.getDer(), difExtremos);
+	}
+	
+	public int mayoresNoHojas(T elem) {
+		if(getRaiz() == null)
+			return 0;
+		return mayoresNoHojas(getRaiz(), elem);
+	}
+	private int mayoresNoHojas(Nodo<T> nodo, T elem) {
+		if(nodo == null)
+			return 0;
+		if((nodo.getIzq() != null || nodo.getDer() != null) && nodo.getInfo().compareTo(elem) > 0)
+			return 1 + mayoresNoHojas(nodo.getIzq(), elem) + mayoresNoHojas(nodo.getDer(), elem);
+		return mayoresNoHojas(nodo.getIzq(), elem) + mayoresNoHojas(nodo.getDer(), elem);
+	}
+	public String dosHijos() {
+		if(getRaiz() == null)
+			return "";
+		return dosHijos(getRaiz());
+	}
+	private String dosHijos(Nodo<T> nodo) {
+		if(nodo == null)
+			return "";
+		if(nodo.getIzq() != null && nodo.getDer() != null)
+			return nodo.getInfo() + ", " + dosHijos(nodo.getIzq()) + dosHijos(nodo.getDer());
+		return dosHijos(nodo.getIzq()) + dosHijos(nodo.getDer());
+	}
+	public int sumarSubArbol(int num) {
+		return sumarSubArbol(num, getRaiz());
+	}
+	private int sumarSubArbol(int num, Nodo<Integer> nodo) {
+		if(nodo == null)
+			return 0;
+		if(nodo.getInfo() == num)
+			return sumarElementos(nodo);
+		if(nodo.getInfo() < num)
+			return sumarSubArbol(num, nodo.getDer());
+		else
+			return sumarSubArbol(num, nodo.getIzq());
+	}
+	private int sumarElemetos(Nodo<Integer> nodo) {
+		if(nodo == null)
+			return 0;
+		else
+			return sumarElementos(nodo.getDer().getInfo()) + sumarElementos(nodo.getIzq().getInfo());
+	}
 	
 }
